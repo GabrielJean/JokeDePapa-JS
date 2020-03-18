394c01bb95c50f4e46c6a2333d6222729de9eb46
Gabriel Jean <theelithe555@gmail.com>
2020-03-18 21:57:02 +0000

    updated bot with new command + cli interface

--- bot.js
+++ bot.js
@@ -1,9 +1,22 @@
 const { Client, MessageEmbed } = require('discord.js');
+const prompt = require('prompt');
+const config = require("./config.json");
 
 const client = new Client();
 const fs = require('fs');
 
 const files = fs.readdirSync('./Audio');
+
+
+async function mgmConsole() {
+  prompt.get(['command'], function (err, result) {
+    if (err) { return onErr(err); }
+    if (result.command === "kill") {
+      process.kill()
+    }
+    mgmConsole()
+  });
+}
 
 client.on('ready', () => {
   // eslint-disable-next-line no-console
@@ -22,7 +35,12 @@ client.on('message', async (message) => {
   // we ignore it
   if (!message.guild) return;
 
-  if (message.content === '!joke') {
+  if(message.content.indexOf(config.prefix) !== 0) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if (command === 'joke') {
     // Only try to join the sender's voice channel if they are in one themselves
     if (message.member.voice.channel) {
       const file = files[Math.floor(Math.random() * files.length)];
@@ -55,7 +73,7 @@ client.on('message', async (message) => {
     }
   }
 
-  if (message.content === '!leave') {
+  if (command === "leave") {
     // Only try to join the sender's voice channel if they are in one themselves
     if (message.member.voice.channel) {
       // eslint
@@ -63,7 +81,18 @@ client.on('message', async (message) => {
     }
   }
 
-  if (message.content === '!help') {
+  if(command === "say") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    if (message === ""){return}
+    const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+    message.channel.send(sayMessage);
+  }
+
+  if (command === 'help') {
     const embed = new MessageEmbed()
       // Set the title of the field
       .setTitle('Commandes :')
@@ -73,6 +102,7 @@ client.on('message', async (message) => {
       .addField('!help', 'Affiche ce message')
       .addField('!joke', 'Joue une blague')
       .addField('!ping', 'Pong !')
+      .addField('!say', 'Le bot affiche un message')
       // Set the thumbnail of the embed
       .setThumbnail(
         'https://cdn.shoplightspeed.com/shops/612132/files/6072039/randolph-jokes-de-papa-le-jeu-de-societe.jpg',
@@ -82,7 +112,7 @@ client.on('message', async (message) => {
   }
 });
 
-settings = fs.readFileSync("settings.json", "utf8")
-settings = JSON.parse(settings)
-const token = settings["token"]
-client.login(token);
+setTimeout(function () {
+  mgmConsole(); 
+}, 1000);
+client.login(config.token)
